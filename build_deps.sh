a8805a6ada9084e545d14a859927126cffd8f471
zouguowei <guowzou@gmail.com>
2023-09-12 21:12:48 +0800

    add libuv library

---
+++ build_deps.sh
@@ -0,0 +1,11 @@
+git submodule update --init --recursive
+
+export script_path=`pwd`
+mkdir -p $script_path/thirdparty/build 
+
+cd $script_path/thirdparty/libuv
+mkdir build
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DLIBUV_BUILD_SHARED=OFF
+cmake --build build --config Release
+cmake --install build --prefix $script_path/thirdparty/build
+cd $script_path
