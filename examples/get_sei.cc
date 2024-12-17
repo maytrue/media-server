4d2730feedd61264173abc99c506f3a1317e97e1
zouguowei <zouguowei@kuaishou.com>
2024-12-17 09:43:14 +0800

    modify sei

--- examples/get_sei.cc
+++ examples/get_sei.cc
@@ -2,17 +2,18 @@
 
 extern "C" {
 #include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
 }
 
 void extract_sei(AVPacket *pkt) {
   uint8_t *data = pkt->data;
   int size = pkt->size;
   int nal_type = 0;
-  std::cout << "extract_sei" << std::endl;
+  // std::cout << "extract_sei size:" << size << std::endl;
   while (size > 4) {
-    if (data[0] == 0 && data[1] == 0 && data[2] == 0 && data[3] == 1) {
-      nal_type = (data[4] & 0x1F); // For H.264
-      std::cout << "nal_type:" << nal_type;
+    if (data[0] == 0 && data[1] == 0 && data[2] == 1) {
+      nal_type = (data[3] & 0x1F); // For H.264
+      std::cout << "nal_type:" << nal_type << std::endl;
     }
 
     data++;
